54c918848d1dffcd6c797ba604a39b47bbe37e5c
kuaniyeung <kuaniyeung@gmail.com>
2024-08-28 14:33:48 -0400

    chore: pinia setup

---
+++ src/stores/use-auth-store/index.ts
@@ -0,0 +1,24 @@
+import { defineStore, StateTree } from "pinia";
+
+interface State extends StateTree {
+  user: Record<string, any> | null;
+}
+export default (storeId = "auth") => {
+  const store = defineStore(storeId, {
+    state: () =>
+      ({
+        user: null,
+      } as State),
+    actions: {
+      async getUser() {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const json = await response.json();
+        console.log(json);
+      },
+    },
+  });
+
+  return store();
+};
