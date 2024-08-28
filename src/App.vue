5b0c46d49700119908a7502e5f824a8616364d74
Stephanie Yeung <97987114+kuaniyeung@users.noreply.github.com>
2024-08-28 14:34:13 -0400

    chore: pinia setup (#3)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
   <button className="btn btn-warning">Warning</button>
 </template>
+
+<script setup lang="ts">
+import { onMounted, watch } from "vue";
+import { useAuthStore } from "./stores";
+import { storeToRefs } from "pinia";
+
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
+
+onMounted(() => {
+  authStore.getUser();
+});
+</script>
 
 <style scoped></style>
