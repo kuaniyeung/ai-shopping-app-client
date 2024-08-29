cab4e3ebd351071ed63c06bc124f4e2b0a146266
Stephanie Yeung <97987114+kuaniyeung@users.noreply.github.com>
2024-08-29 17:09:07 -0400

    task: created button component (#6)

--- src/components/controls/button/Button.stories.ts
+++ src/components/controls/button/Button.stories.ts
@@ -1,32 +1,66 @@
+import { Meta, StoryFn } from "@storybook/vue3";
 import Button from "./Button.vue";
-import type { Meta, StoryObj } from "@storybook/vue3";
-import { fn } from "@storybook/test";
+import { ref } from "vue";
+import { ButtonStyles } from "./enums";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta = {
-  title: "Example/Button-Hey",
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
   component: Button,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ["autodocs"],
-  argTypes: {},
-  args: {
-    label: "Hey bb",
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  argTypes: {
+    label: { control: "text" },
+    loading: { control: "boolean" },
+    iconStart: { control: "text" },
+    iconEnd: { control: "text" },
   },
-} satisfies Meta<typeof Button>;
-export default meta;
-type Story = StoryObj<typeof meta>;
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/api/csf
- * to learn how to use render functions.
- */
-export const Primary: Story = {
-  args: {},
 };
 
-export const Secondary: Story = {
-  args: {
-    label: "Secondary",
+export default meta;
+
+const Template: StoryFn<typeof Button> = (args) => ({
+  components: { Button },
+  setup() {
+    const loading = ref(args.loading);
+    return { args, loading };
   },
+  template: `
+    <Button v-bind="args">
+      <template #iconStart>
+        <span v-if="args.iconStart">{{ args.iconStart }}</span>
+      </template>
+      <template #iconEnd>
+        <span v-if="args.iconEnd">{{ args.iconEnd }}</span>
+      </template>
+    </Button>
+  `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "Click Me",
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  label: "Loading...",
+  loading: true,
+};
+
+export const WithIcons = Template.bind({});
+WithIcons.args = {
+  label: "With Icons",
+  iconStart: "🔍",
+  iconEnd: "🚀",
+};
+
+export const IconButton = Template.bind({});
+IconButton.args = {
+  iconStart: "🔍",
+  class: ButtonStyles.ICON,
+};
+
+export const DisabledButton = Template.bind({});
+DisabledButton.args = {
+  label: "Disabled",
+  //@ts-ignore
+  disabled: true,
 };
--- src/components/controls/button/index.spec.ts
+++ src/components/controls/button/index.spec.ts
@@ -1,16 +1,84 @@
-import { test, expect, describe } from "vitest";
-import { mount } from "@vue/test-utils";
-import { Button } from "..";
+import { test, expect, describe, beforeEach } from "vitest";
+import { mount, VueWrapper } from "@vue/test-utils";
+import { Button, ButtonThemes } from "..";
+
+const initWrapper = (options: Object = {}) => {
+  const wrapper = mount(Button, {
+    props: {
+      label: "Hey darling",
+    },
+    ...options,
+  });
+  return wrapper;
+};
 
 describe("Button", () => {
+  let wrapper: VueWrapper<InstanceType<typeof Button>>;
+  const getLabel = () => wrapper.find("[data-test=label]");
+  const getLoader = () => wrapper.find("[data-test=loader]");
+
+  const mountComponent = (options = {}) => (wrapper = initWrapper(options));
+  beforeEach(() => {
+    mountComponent();
+  });
+
   describe("Template", () => {
-    test(`should render with correct text`, async () => {
-      const wrapper = mount(Button, {
-        props: {
-          label: "Hey darling",
-        },
+    test(`should have correct attrs && props`, async () => {
+      const attrs = wrapper.attributes();
+      expect(attrs).toHaveProperty("class", `btn ${ButtonThemes.PRIMARY}`);
+    });
+    describe("label", () => {
+      test(`should render with correct text`, () => {
+        expect(getLabel().exists()).toBeTruthy();
+        expect(getLabel().text()).toEqual(wrapper.props().label);
       });
-      expect(wrapper.text()).toBe(wrapper.props().label);
+      test(`should not render if props.label is falsy`, async () => {
+        wrapper.setProps({ label: "" });
+        await wrapper.vm.$nextTick();
+        expect(getLabel().exists()).toBeFalsy();
+      });
+    });
+
+    describe("loader", () => {
+      test(`should render if props.loading is truthy`, async () => {
+        expect(getLoader().exists()).toBeFalsy();
+        wrapper.setProps({ loading: true });
+        await wrapper.vm.$nextTick();
+        expect(getLoader().exists()).toBeTruthy();
+      });
+    });
+
+    describe("$slots", () => {
+      const icon = '<i class="icon"></i>';
+      describe("slots.iconStart", () => {
+        beforeEach(() => {
+          mountComponent({ slots: { iconStart: icon } });
+        });
+        test(`should render if $slots.iconStart is passed to the component`, async () => {
+          expect(wrapper.html()).contains(icon);
+        });
+
+        test(`should not render if props.loading is truthy`, async () => {
+          wrapper.setProps({ loading: true });
+          await wrapper.vm.$nextTick();
+          expect(wrapper.html()).not.contains(icon);
+        });
+      });
+
+      describe("slots.iconEnd", () => {
+        beforeEach(() => {
+          mountComponent({ slots: { iconEnd: icon } });
+        });
+        test(`should render if $slots.iconEnd is passed to the component`, async () => {
+          expect(wrapper.html()).contains(icon);
+        });
+
+        test(`should not render if props.loading is truthy`, async () => {
+          wrapper.setProps({ loading: true });
+          await wrapper.vm.$nextTick();
+          expect(wrapper.html()).not.contains(icon);
+        });
+      });
     });
   });
 });
