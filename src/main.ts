b647483936f73cdac647a090e72b8512bf3da30d
Stephanie Yeung <97987114+kuaniyeung@users.noreply.github.com>
2024-08-29 11:54:30 -0400

    chore: init storybook (#4)

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./index.css";
 import App from "./App.vue";
+import { Button } from "./components";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
+app.component("Button", Button);
 app.mount("#app");
