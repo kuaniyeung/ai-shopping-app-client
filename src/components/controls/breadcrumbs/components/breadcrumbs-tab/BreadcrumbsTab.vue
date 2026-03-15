651cc675bd4e7b3c9b52c3e0f42f0a8a0c81e08a
Stephanie Yeung <97987114+kuaniyeung@users.noreply.github.com>
2026-03-15 13:55:38 -0400

    chore: update breadcrumb global compoenent (#13)

--- src/components/controls/breadcrumbs/components/breadcrumbs-tab/BreadcrumbsTab.vue
+++ src/components/controls/breadcrumbs/components/breadcrumbs-tab/BreadcrumbsTab.vue
@@ -1,16 +1,3 @@
-<template>
-  <li>
-    <a v-if="!disabled" :href="href" data-test="title-enabled-link">
-      {{ title }}</a
-    >
-    <span v-else data-test="title-disabled-link"> {{ title }}</span>
-  </li>
-</template>
+<template></template>
 
-<script setup lang="ts">
-defineProps({
-  title: { type: String, required: true },
-  disabled: { type: Boolean, default: true },
-  href: { type: String },
-});
-</script>
+<script setup lang="ts"></script>
